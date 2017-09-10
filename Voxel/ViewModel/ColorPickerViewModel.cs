5915b6efd437a39186f72cb521fb5fe85e137180
Grant Howard <the1812@outlook.com>
2017-09-10 14:21:03 +0800

    Add HexColor.set, bind to textHexColor

--- Voxel/ViewModel/ColorPickerViewModel.cs
+++ Voxel/ViewModel/ColorPickerViewModel.cs
@@ -1,3 +1,4 @@
+using Ace;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +13,15 @@ namespace Voxel.ViewModel
     {
         public ColorPickerViewModel() : base(new ColorPickerLanguage()) { }
 
+#region Lanugage
         public string WindowTitle => language[nameof(WindowTitle)];
         public string ButtonOK => language[nameof(ButtonOK)];
         public string ButtonCancel => language[nameof(ButtonCancel)];
         public string TextSample => language[nameof(TextSample)];
-
-
-
-        private Color selectedColor = Ace.Wpf.DwmEffect.ColorizationColor;
+        #endregion
+        #region Vars and properties
+        private static Color dwmColor = Ace.Wpf.DwmEffect.ColorizationColor;
+        private Color selectedColor = dwmColor;
         public Color SelectedColor
         {
             get => selectedColor;
@@ -27,10 +29,53 @@ namespace Voxel.ViewModel
             {
                 selectedColor = value;
                 OnPropertyChanged(nameof(SelectedColor));
+                OnPropertyChanged(nameof(HexColor));
             }
         }
 
-        
+        public string HexColor
+        {
+            get
+            {
+                return $"{selectedColor.R:X2}{selectedColor.G:X2}{selectedColor.B:X2}";
+            }
+            set
+            {
+                string hex = value;
+                if (hex.Length == 3)
+                {
+                    hex = $"{hex[0]}{hex[0]}{hex[1]}{hex[1]}{hex[2]}{hex[2]}";
+                }
+
+                if (hex.Length == 6 && hex.IsMatch("[A-Fa-f0-9]*"))
+                {
+                    var color = Color.FromArgb(
+                        255,
+                        Convert.ToByte(hex.Substring(0, 2), 16),
+                        Convert.ToByte(hex.Substring(2, 2), 16),
+                        Convert.ToByte(hex.Substring(4, 2), 16)
+                        );
+                    selectedColor = color;
+                    OnPropertyChanged(nameof(SelectedColor));
+                    OnPropertyChanged(nameof(HexColor));
+                }
+                else
+                {
+                    selectedColor = dwmColor;
+                    OnPropertyChanged(nameof(SelectedColor));
+                    OnPropertyChanged(nameof(HexColor));
+                }
+            }
+        }
+        #endregion
+        #region Commands
+
+        #endregion
+
+
+
+
+
 
     }
 }
